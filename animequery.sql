67fa5a8895c180e4517449143bbaf037a86b80be
NytKing <86428423+NytKing@users.noreply.github.com>
2022-08-29 17:01:47 +0530

    Update animequery.sql

--- animequery.sql
+++ animequery.sql
@@ -1,4 +1,6 @@
-SELECT DISTINCT value AS genre,dbo.Sheet1$.uid,title,episodes,members,popularity,ranked,score,CASE WHEN popularity >= 10000 THEN 'Less popular' WHEN (popularity >= 5000 AND popularity < 10000) THEN 'okaishly popular' WHEN popularity < 5000 THEN 'Very popular' END AS category
+SELECT DISTINCT value AS genre,dbo.Sheet1$.uid,title,episodes,members,popularity,ranked,score,
+CASE WHEN popularity >= 10000 THEN 'Less popular' WHEN (popularity >= 5000 AND popularity < 10000) 
+THEN 'okaishly popular' WHEN popularity < 5000 THEN 'Very popular' END AS category
 
 FROM (SELECT DISTINCT value,uid, RANK() OVER(PARTITION BY uid ORDER BY value) AS position  
 	  FROM dbo.Sheet1$
