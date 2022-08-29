25b21f21b3c655be04642fb9018a71f608e30553
NytKing <86428423+NytKing@users.noreply.github.com>
2022-08-29 17:00:45 +0530

    Add files via upload

---
+++ animequery.sql
@@ -0,0 +1,17 @@
+--exec sp_help 'dbo.Sheet1$'
+
+/*UPDATE dbo.Sheet1$
+SET score = 0
+WHERE score  IS NULL*/
+
+--USE anime
+
+SELECT DISTINCT value AS genre,dbo.Sheet1$.uid,title,episodes,members,popularity,ranked,score,CASE WHEN popularity >= 10000 THEN 'Less popular' WHEN (popularity >= 5000 AND popularity < 10000) THEN 'okaishly popular' WHEN popularity < 5000 THEN 'Very popular' END AS category
+
+FROM (SELECT DISTINCT value,uid, RANK() OVER(PARTITION BY uid ORDER BY value) AS position  
+	  FROM dbo.Sheet1$
+	  CROSS APPLY string_split(genre,' ')
+	  WHERE value != ' ') AS new, dbo.Sheet1$ 
+
+WHERE dbo.Sheet1$.uid = new.uid AND position = 1 --AND aired = 'Not available'
+ORDER BY members 
